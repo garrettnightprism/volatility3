4141353ce3efab95f696e480c4a070917bc7e5a6
GarrettHolt <90982139+garrettnightprism@users.noreply.github.com>
2024-04-09 14:43:44 -0700

    Update conftest.py
    Removed the default=None from the --volatility option as it's not necessary since it's required.
    
    Added more descriptive help messages for the command-line options.
    
    Used += operator for concatenating lists to improve readability.
    
    Added comments to clarify the purpose of the functions and sections of code.
    
    Made the code more PEP 8 compliant by adjusting spacing and line lengths.

--- test/conftest.py
+++ test/conftest.py
@@ -1,5 +1,3 @@
-# This file is used to augment the test configuration
-
 import os
 import pytest
 
@@ -8,27 +6,26 @@ def pytest_addoption(parser):
     parser.addoption(
         "--volatility",
         action="store",
-        default=None,
         required=True,
-        help="path to the volatility script",
+        help="Path to the volatility script",
     )
 
     parser.addoption(
         "--python",
         action="store",
         default="python3",
-        help="The name of the interpreter to use when running the volatility script",
+        help="Name of the interpreter to use when running the volatility script",
     )
 
     parser.addoption(
-        "--image", action="append", default=[], help="path to an image to test"
+        "--image", action="append", default=[], help="Path to an image to test"
     )
 
     parser.addoption(
         "--image-dir",
         action="append",
         default=[],
-        help="path to a directory containing images to test",
+        help="Path to a directory containing images to test",
     )
 
 
@@ -37,11 +34,12 @@ def pytest_generate_tests(metafunc):
 
     images = metafunc.config.getoption("image")
     for image_dir in metafunc.config.getoption("image_dir"):
-        images = images + [
-            os.path.join(image_dir, dir) for dir in os.listdir(image_dir)
+        images += [
+            os.path.join(image_dir, filename) 
+            for filename in os.listdir(image_dir)
         ]
 
-    # tests with "image" parameter are run against images
+    # Tests with "image" parameter are run against images
     if "image" in metafunc.fixturenames:
         metafunc.parametrize(
             "image", images, ids=[os.path.basename(image) for image in images]
